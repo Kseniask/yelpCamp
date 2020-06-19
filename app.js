eb70b0348b20f52d3daa9b0017a74f61562172ea
kseniask <kseniask30@icloud.com>
2020-06-19 11:15:19 -0700

    DOM structrure and bootstrap added

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ var campgrounds = [
     name: "Mountain Goat's Residence",
     image:
       'https://landwithoutlimits.com/resources/uploads/2019/10/CCCTMA-Images-Camping-CCC-Brad-Kasselman-v2-2048x1152.jpg'
+  },
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://hipcamp-res.cloudinary.com/images/c_fill,f_auto,g_auto,h_504,q_60,w_770/v1445485302/campground-photos/cznqogta0xm6pynikxeo/salmon-creek-ranch-redwood-camp-bay-area-tent-people-lodging.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image:
+      'https://swoop-patagonia.imgix.net/SWO_4_TOM_ALL_Paine_Grande_Campsite_HEADER_.JPG?auto=format,enhance,compress&fit=crop&w=1200&h=0&q=30'
+  },
+  {
+    name: "Mountain Goat's Residence",
+    image:
+      'https://landwithoutlimits.com/resources/uploads/2019/10/CCCTMA-Images-Camping-CCC-Brad-Kasselman-v2-2048x1152.jpg'
   }
 ]
 
